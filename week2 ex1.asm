cb1d3819f468aed616f78b1e232074cac713fb81
Sathvikasomu <sathvika_somu@srmap.edu.in>
2024-08-22 10:56:54 +0530

    week2 ex1.asm

--- week2 ex1.asm
+++ week2 ex1.asm
@@ -1,5 +1,33 @@
-org 100h
-mov al,30h
-mov ah,32h
-add ah,al
+org 100h 
+jmp start
+msg:db "addition of two 8-bit numbers is:", 0Dh,0Ah,24h
+start: mov dx,msg
+       mov ah,09h
+       int 21h
+
+num1 db 30h
+num2 db 32h
+mov al,num1
+add al,num2
+mov bl,al
+AND al,0F0h
+shr al, 4
+add al,30h
+cmp al,39h
+jle first_bit
+add al,7h
+first_bit: mov dl,al
+           mov ah,02h
+           int 21h
+mov al,bl
+and al,0Fh
+add al,30h
+cmp al,39h
+jle second_bit
+add al,7h
+second_bit: mov dl,al
+            mov ah,02h
+            int 21h  
 ret
+
+      
